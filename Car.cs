da99272c2cbe26133d309be548b363c0e5e237d0
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2025-09-29 10:55:50 +0300

    Create Car class

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿namespace TollApp
+{
+    public class Car
+    {
+        private static Random rng = new Random();
+        private static int counter = 0;
+        private const string letters =
+            "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЭЮЯ";
+        public string LicenseNumber { get; set; }
+        public int Id { get; set; }
+        public Color Color { get; set; }
+        public Car()
+        {
+            Id = ++counter;
+            Color = Color.FromArgb(
+                rng.Next(256), rng.Next(256), rng.Next(256));
+            int len = letters.Length;
+            LicenseNumber =
+                letters[rng.Next(len)].ToString()
+                + (char)rng.Next(10)
+                + (char)rng.Next(10)
+                + letters[rng.Next(len)];
+        }
+        public override string ToString()
+        {
+            return LicenseNumber;
+        }
+    }
+}
